ac70f0efc5ae4c64f80ab6acadb32378ce959a81
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2024-12-16 15:02:48 +0800

    tried html dom

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,5 +1,13 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Paragraph changed!";
+}
+
+function updateProfilePicture() {
+  document.getElementById("profilePicture").src = "https://github.com/rainvp/Personal-Profile-Webpage/blob/main/images/%F0%9D%92%AE%F0%9D%92%B6%F0%9D%92%BE%F0%9D%93%81%E2%84%B4%F0%9D%93%87%20%F0%9D%93%82%E2%84%B4%E2%84%B4%F0%9D%93%83%20%E2%8B%86%E2%98%BE%C2%B7%CC%A9%CD%99.jpg?raw=true";
+}
+
+function displayDateTime() {
+  document.getElementById("dateTime").innerHTML = "Current Date and Time: " + new Date();
+}
 
   
